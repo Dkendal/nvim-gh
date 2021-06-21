36ffb6fe259b91b590670bdf3a50a5450aedba17
Dylan Kendal <dylankendal@gmail.com>
2021-06-20 21:15:13 -0400

    feat: add mappings for `run list`

--- fnl/gh.fnl
+++ fnl/gh.fnl
@@ -7,7 +7,7 @@
 (local api vim.api)
 (local ex vim.cmd)
 (local v vim.fn)
-(local bmap vim.api.nvim_buf_set_keymap)
+(local bmap (partial vim.api.nvim_buf_set_keymap 0))
 
 (fn hist-push [s]
   (table.insert history s))
@@ -32,10 +32,9 @@
 (fn gh.next-line []
   (print :next))
 
-(fn gh.sub-cmd [cmd]
-  (let [pr-num (-> (api.nvim_get_current_line) (string.match "^%s+#(%d+)"))]
-    (when pr-num
-      (ex (.. "Gh " (string.format cmd pr-num))))))
+(fn fnref [name ?args]
+  (local args (string.gsub (vim.inspect (or ?args {})) "%s+" " "))
+  (.. ":lua require('gh')['" name "'](table.unpack(" args "))<cr>"))
 
 (fn gh.pr-view []
   (let [pr-num (-> (api.nvim_get_current_line) (string.match "^%s+#(%d+)"))]
@@ -47,9 +46,20 @@
     (when pr-num
       (ex (.. "Gh pr diff " pr-num)))))
 
-(fn fnref [name ?args]
-  (local args (string.gsub (vim.inspect (or ?args {})) "%s+" " "))
-  (.. ":lua require('gh')['" name "'](table.unpack(" args "))<cr>"))
+(local pr-pattern "^%(%d+)")
+
+(fn gh.pr-sub-cmd [cmd]
+  (let [id (-> (api.nvim_get_current_line) (string.match pr-pattern))]
+    (when id
+      (ex (.. "Gh " (string.format cmd id))))))
+
+;; X  debugging                                     .github/workflows/ci.yml  feat-ci-2  push          938222443
+(local run-pattern "%s(%d+)$")
+
+(fn gh.run-sub-cmd [cmd]
+  (let [id (-> (api.nvim_get_current_line) (string.match run-pattern))]
+    (when id
+      (ex (.. "Gh " (string.format cmd id))))))
 
 (fn gh.keymap []
   (let [tokens ;
@@ -58,19 +68,23 @@
             (stringx.split))
         opts {:nowait true :silent true}]
     ;; Map for all modes
-    (bmap 0 :n :q (fnref :hist-back) opts)
+    (bmap :n :q (fnref :hist-back) opts)
     (match tokens
+      [:gh :run :list _]
+      ;; Actions
+      (do
+        (bmap :n :<enter> (fnref :run-sub-cmd ["run view %s"]) opts))
       [:gh :pr :status _]
       ;: Pr Status maps
       (do
-        (bmap 0 :n :<enter> (fnref :sub-cmd ["pr view %s --comments"]) opts)
-        (bmap 0 :n :h (fnref :sub-cmd ["pr --help"]) opts)
-        (bmap 0 :n :c (fnref :sub-cmd ["pr checks %s"]) opts)
-        (bmap 0 :n :CC (fnref :sub-cmd ["pr checkout %s"]) opts)
-        (bmap 0 :n :l (fnref :sub-cmd ["pr list"]) opts)
-        (bmap 0 :n :d (fnref :sub-cmd ["pr diff %s"]) opts)
-        (bmap 0 :n :o (fnref :sub-cmd ["pr view %s --web"]) opts)
-        (bmap 0 :n :n (fnref :next-line) opts)))))
+        (bmap :n :<enter> (fnref :pr-sub-cmd ["pr view %s --comments"]) opts)
+        (bmap :n :h (fnref :pr-sub-cmd ["pr --help"]) opts)
+        (bmap :n :c (fnref :pr-sub-cmd ["pr checks %s"]) opts)
+        (bmap :n :CC (fnref :pr-sub-cmd ["pr checkout %s"]) opts)
+        (bmap :n :l (fnref :pr-sub-cmd ["pr list"]) opts)
+        (bmap :n :d (fnref :pr-sub-cmd ["pr diff %s"]) opts)
+        (bmap :n :o (fnref :pr-sub-cmd ["pr view %s --web"]) opts)
+        (bmap :n :n (fnref :next-line) opts)))))
 
 (fn gh.setup []
   (do
--- lua/gh.lua
+++ lua/gh.lua
@@ -4,7 +4,11 @@ local history = {}
 local api = vim.api
 local ex = vim.cmd
 local v = vim.fn
-local bmap = vim.api.nvim_buf_set_keymap
+local bmap
+local function _0_(...)
+  return vim.api.nvim_buf_set_keymap(0, ...)
+end
+bmap = _0_
 local function hist_push(s)
   return table.insert(history, s)
 end
@@ -29,11 +33,9 @@ end
 gh["next-line"] = function()
   return print("next")
 end
-gh["sub-cmd"] = function(cmd)
-  local pr_num = string.match(api.nvim_get_current_line(), "^%s+#(%d+)")
-  if pr_num then
-    return ex(("Gh " .. string.format(cmd, pr_num)))
-  end
+local function fnref(name, _3fargs)
+  local args = string.gsub(vim.inspect((_3fargs or {})), "%s+", " ")
+  return (":lua require('gh')['" .. name .. "'](table.unpack(" .. args .. "))<cr>")
 end
 gh["pr-view"] = function()
   local pr_num = string.match(api.nvim_get_current_line(), "^%s+#(%d+)")
@@ -47,25 +49,38 @@ gh["pr-diff"] = function()
     return ex(("Gh pr diff " .. pr_num))
   end
 end
-local function fnref(name, _3fargs)
-  local args = string.gsub(vim.inspect((_3fargs or {})), "%s+", " ")
-  return (":lua require('gh')['" .. name .. "'](table.unpack(" .. args .. "))<cr>")
+local pr_pattern = "^%(%d+)"
+gh["pr-sub-cmd"] = function(cmd)
+  local id = string.match(api.nvim_get_current_line(), pr_pattern)
+  if id then
+    return ex(("Gh " .. string.format(cmd, id)))
+  end
+end
+local run_pattern = "%s(%d+)$"
+gh["run-sub-cmd"] = function(cmd)
+  local id = string.match(api.nvim_get_current_line(), run_pattern)
+  if id then
+    return ex(("Gh " .. string.format(cmd, id)))
+  end
 end
 gh.keymap = function()
   local tokens = stringx.split(string.match(v.expand("%:t"), "%d*:(.*)"))
   local opts = {nowait = true, silent = true}
-  bmap(0, "n", "q", fnref("hist-back"), opts)
-  local _0_ = tokens
-  if ((type(_0_) == "table") and ((_0_)[1] == "gh") and ((_0_)[2] == "pr") and ((_0_)[3] == "status") and true) then
-    local _ = (_0_)[4]
-    bmap(0, "n", "<enter>", fnref("sub-cmd", {"pr view %s --comments"}), opts)
-    bmap(0, "n", "h", fnref("sub-cmd", {"pr --help"}), opts)
-    bmap(0, "n", "c", fnref("sub-cmd", {"pr checks %s"}), opts)
-    bmap(0, "n", "CC", fnref("sub-cmd", {"pr checkout %s"}), opts)
-    bmap(0, "n", "l", fnref("sub-cmd", {"pr list"}), opts)
-    bmap(0, "n", "d", fnref("sub-cmd", {"pr diff %s"}), opts)
-    bmap(0, "n", "o", fnref("sub-cmd", {"pr view %s --web"}), opts)
-    return bmap(0, "n", "n", fnref("next-line"), opts)
+  bmap("n", "q", fnref("hist-back"), opts)
+  local _1_ = tokens
+  if ((type(_1_) == "table") and ((_1_)[1] == "gh") and ((_1_)[2] == "run") and ((_1_)[3] == "list") and true) then
+    local _ = (_1_)[4]
+    return bmap("n", "<enter>", fnref("run-sub-cmd", {"run view %s"}), opts)
+  elseif ((type(_1_) == "table") and ((_1_)[1] == "gh") and ((_1_)[2] == "pr") and ((_1_)[3] == "status") and true) then
+    local _ = (_1_)[4]
+    bmap("n", "<enter>", fnref("pr-sub-cmd", {"pr view %s --comments"}), opts)
+    bmap("n", "h", fnref("pr-sub-cmd", {"pr --help"}), opts)
+    bmap("n", "c", fnref("pr-sub-cmd", {"pr checks %s"}), opts)
+    bmap("n", "CC", fnref("pr-sub-cmd", {"pr checkout %s"}), opts)
+    bmap("n", "l", fnref("pr-sub-cmd", {"pr list"}), opts)
+    bmap("n", "d", fnref("pr-sub-cmd", {"pr diff %s"}), opts)
+    bmap("n", "o", fnref("pr-sub-cmd", {"pr view %s --web"}), opts)
+    return bmap("n", "n", fnref("next-line"), opts)
   end
 end
 gh.setup = function()
